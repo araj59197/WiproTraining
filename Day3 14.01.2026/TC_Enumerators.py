51642c1349566b6b3652b8d58904e246143e8092
Aditya Raj <araj59197@gmail.com>
2026-01-14 16:25:32 +0530

    second  commit

--- Day3 14.01.2026/TC_Enumerators.py
+++ Day3 14.01.2026/TC_Enumerators.py
@@ -4,8 +4,12 @@ fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"]
 for index, fruit in enumerate(fruits):
     print(f"Index {index}: {fruit}")
 print("\nStarting enumeration from index 1:")
-for index, fruit in enumerate(fruits, start=1):
-    print(f"Index {index}: {fruit}")
-print("\nCreating a dictionary from enumerated fruits:")
-fruit_dict = dict(enumerate(fruits))
-print(fruit_dict)
+
+
+from enum import Enum
+class color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+print(color.RED.value)
+print(color.GREEN.name)
